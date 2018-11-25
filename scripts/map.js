a8b8f1d22d35f46ba3e04deb5213512414651bc2
Alberto <33761650+AlbertQM@users.noreply.github.com>
2018-11-25 23:55:05 +0000

    Added Mood filter overlay (#11)
    * Added mood selector overlay
    
    * Added style for mood selector overlay
    
    * Updated mood colours to match icons
    
    * Updated removeMarkers()
    
    * Removed deprecated testing meethod
    
    * Added moods filter method
    
    * Removed deprecated code

--- scripts/map.js
+++ scripts/map.js
@@ -1,16 +1,17 @@
 let moodColour; //Obj containing mood:colour
 let data;       //Used to store a copy of json - so we modify the copy, not the original
+let adventurous, hungry, creative, tired, romantic, sad;
 
 function setup() {
   data = json;
   setIsLiked('central','hydepark', 'yes')
   noCanvas();
-  moodColour = {adventurous: "BLUE", hungry: "PURPLE", creative: "ORANGE", tired: "GREEN", romantic: "RED", sad: "YELLOW"}
+  moodColour = {adventurous: "rgba(66, 173, 244, 0.65)", hungry: "rgba(118, 69, 209, 0.65)", creative: "rgba(229, 150, 22, 0.65)", tired: "rgba(56, 181, 97, 0.65)", romantic: "rgba(218, 118, 104, 0.65)", sad: "rgba(244, 205, 65, 0.65)"}
+  let map = createMap(51.509865, -0.118092);//Creates a map centered in London center
+  setupMoodFilter(map);
 }
 
 function draw() {
-  let map = createMap(51.509865, -0.118092);//Creates a map centered in London center
-  all = setupMarkersByMood(map, 'adventurous');
   noLoop();
 }
 
@@ -151,14 +152,104 @@ function setIsVisited(zone, place, isVisited) {
   data[zone][0][place].isLiked = isVisited;
 }
 
-function removeMarkers(markersArray) {//Removes all markers
-  for (let marker of markersArray) {
-    marker.remove()
+function removeMarkers(markersArray) {//Removes all markers of a mood
+  if(markersArray != null) {
+    for (let marker of markersArray) {
+      marker.remove()
+    }
   }
 }
 
-function keyPressed() {//Test interaction
-  if (keyCode === DELETE) {
-    removeMarkers(central);
-  }
+
+function setupMoodFilter(map) {
+  $('#adventMood').click(e => {
+    if (!$(e.target).closest('.moodsConts').hasClass('selected')) {
+      adventurous = setupMarkersByMood(map, 'adventurous');
+    }
+    $(e.target).closest('.moodsConts').toggleClass('selected')
+    $('.moodsConts').not('#adventMood').each((idx, mood) => {
+      $(mood).removeClass('selected')
+    })
+    removeMarkers(hungry);
+    removeMarkers(sad);
+    removeMarkers(romantic);
+    removeMarkers(creative);
+    removeMarkers(tired);
+  })
+
+  $('#sadMood').click(e => {
+    if (!$(e.target).closest('.moodsConts').hasClass('selected')) {
+      sad = setupMarkersByMood(map, 'sad');
+    }
+    $(e.target).closest('.moodsConts').toggleClass('selected')
+    $('.moodsConts').not('#romanticMood').each((idx, mood) => {
+      $(mood).removeClass('selected')
+    })
+    removeMarkers(adventurous);
+    removeMarkers(romantic);
+    removeMarkers(hungry);
+    removeMarkers(creative);
+    removeMarkers(tired);
+  })
+
+  $('#romanticMood').click(e => {
+    if (!$(e.target).closest('.moodsConts').hasClass('selected')) {
+      romantic = setupMarkersByMood(map, 'romantic');
+    }
+    $(e.target).closest('.moodsConts').toggleClass('selected')
+    $('.moodsConts').not('#romanticMood').each((idx, mood) => {
+      $(mood).removeClass('selected')
+    })
+    removeMarkers(sad);
+    removeMarkers(adventurous);
+    removeMarkers(hungry);
+    removeMarkers(creative);
+    removeMarkers(tired);
+  })
+
+  $('#tiredMood').click(e => {
+    if (!$(e.target).closest('.moodsConts').hasClass('selected')) {
+      tired = setupMarkersByMood(map, 'tired');
+    }
+    $(e.target).closest('.moodsConts').toggleClass('selected')
+    $('.moodsConts').not('#tiredMood').each(mood => {
+      $(mood).removeClass('selected')
+    })
+    removeMarkers(sad);
+    removeMarkers(romantic);
+    removeMarkers(hungry);
+    removeMarkers(creative);
+    removeMarkers(adventurous);
+  })
+
+  $('#hungryMood').click(e => {
+    if (!$(e.target).closest('.moodsConts').hasClass('selected')) {
+      hungry = setupMarkersByMood(map, 'hungry');
+    }
+    $(e.target).closest('.moodsConts').toggleClass('selected')
+    $('.moodsConts').not('#romanticMood').each((idx, mood) => {
+      $(mood).removeClass('selected')
+    })
+    removeMarkers(adventurous);
+    removeMarkers(sad);
+    removeMarkers(romantic);
+    removeMarkers(creative);
+    removeMarkers(tired);
+  })
+
+  $('#creativeMood').click(e => {
+    if (!$(e.target).closest('.moodsConts').hasClass('selected')) {
+      creative = setupMarkersByMood(map, 'creative');
+    }
+    $(e.target).closest('.moodsConts').toggleClass('selected')
+    $('.moodsConts').not('#romanticMood').each((idx, mood) => {
+      $(mood).removeClass('selected')
+    })
+    removeMarkers(sad);
+    removeMarkers(romantic);
+    removeMarkers(hungry);
+    removeMarkers(adventurous);
+    removeMarkers(tired);
+  })
+
 }
