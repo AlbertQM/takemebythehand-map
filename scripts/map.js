AlbertQM/takemebythehand-map

commit b304c6d0f9cfc9579ed643b72d3ce2dd11bc3e32

Added Place Settings modal and functionalities (#12)
* Added bootstrap CDN

* Added modal DOM and script

* var data now uses localStorage to retain information after page refresh

* App css refactor

* Changed data "No" to "no"

* Finished modal (now displays moods)

* Mood filter post-css refactor update

* Modal now works 100% to store isLiked and isVisited (mood is missing)

* Added modal functionalities (display the correct save state)

index.html
@@ -9,6 +9,12 @@
 
 	<title>InteractiveMap</title>
 
+	<!-- jQuery -->
+	<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+
+	<!-- Bootstrap -->
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 	<!-- STYLE -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" href="./style/map.css">
@@ -19,8 +25,6 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.1/addons/p5.dom.min.js"></script>
 	<script src="/scripts/map.js"></script>
 
-	<!-- jQuery -->
-	<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
 
 	<!-- React.js -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.14.6/react.js"></script>
@@ -40,43 +44,187 @@
 </head>
 
 <body>
+
+	<a class="home-btn m-2" href="#/"><img src="./res/img/icons/Home.png"></a>
+
+	<!--  MOOD FILTER -->
 	<div class="grid-container">
-		<a class="moodsConts" id="adventMood" >
-			<img id="compass" src="./res/img/icons/compass.png">
-			<img id="arrow" src="./res/img/icons/arrow.png">
-			<img id="traveler" src="./res/img/icons/traveler.png">
-		</a>
-		<a class="moodsConts" id="sadMood" >
-			<img id="sadface" src="./res/img/icons/sadface.png">
-			<img id="tear" src="./res/img/icons/tear.png">
-		</a>
-		<a class="moodsConts" id="romanticMood" >
-			<img id="heart" src="./res/img/icons/heart.png">
-			<img id="couple" src="./res/img/icons/couple.png">
-		</a>
-		<a class="moodsConts" id="hungryMood" >
-			<img id="burger1" src="./res/img/icons/burger1.png">
-			<img id="burger2" src="./res/img/icons/burger2.png">
-			<img id="burger3" src="./res/img/icons/burger3.png">
-		</a>
-		<a class="moodsConts" id="tiredMood" >
-			<div class="Zzz">
-				<span class="s2">z</span>
-				<span class="s3">z</span>
-				<span class="s4">z</span>
-				<span class="s5">z</span>
-				<span class="s6">z</span>
-			</div>
-			<img id="tiredMan" src="./res/img/icons/tiredMan.png">
-		</a>
-		<a class="moodsConts" id="creativeMood" >
-			<img id="creativeFace" src="./res/img/icons/creativeFace.png">
-			<img id="gear1" class="spin" src="./res/img/icons/gear.png">
-			<img id="gear2" class="spin-back" src="./res/img/icons/gear.png">
-			<img id="gear3" class="spin" src="./res/img/icons/gear.png">
-		</a>
+	  <a class="moodsConts adventMood" >
+	    <img class="compass" src="./res/img/icons/compass.png">
+	    <img class="arrow" src="./res/img/icons/arrow.png">
+	    <img class="traveler" src="./res/img/icons/traveler.png">
+	  </a>
+	  <a class="moodsConts sadMood" >
+	    <img class="sadface" src="./res/img/icons/sadface.png">
+	    <img class="tear" src="./res/img/icons/tear.png">
+	  </a>
+	  <a class="moodsConts romanticMood" >
+	    <img class="heart" src="./res/img/icons/heart.png">
+	    <img class="couple" src="./res/img/icons/couple.png">
+	  </a>
+	  <a class="moodsConts hungryMood" >
+	    <img class="burger1" src="./res/img/icons/burger1.png">
+	    <img class="burger2" src="./res/img/icons/burger2.png">
+	    <img class="burger3" src="./res/img/icons/burger3.png">
+	  </a>
+	  <a class="moodsConts tiredMood" >
+	    <div class="Zzz">
+	      <span class="s2">z</span>
+	      <span class="s3">z</span>
+	      <span class="s4">z</span>
+	      <span class="s5">z</span>
+	      <span class="s6">z</span>
+	    </div>
+	    <img class="tiredMan" src="./res/img/icons/tiredMan.png">
+	  </a>
+	  <a class="moodsConts creativeMood" >
+	    <img class="creativeFace" src="./res/img/icons/creativeFace.png">
+	    <img class="gear1 spin" src="./res/img/icons/gear.png">
+	    <img class="gear2 spin-back" src="./res/img/icons/gear.png">
+	    <img class="gear3 spin" src="./res/img/icons/gear.png">
+	  </a>
 	</div>
+
+<!-- MODAL -->
+<div class="modal fade" id="changeSettings" tabindex="-1" role="dialog" aria-labelledby="modal-settings" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modal-place_name">'place name' settings</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+			<div class="modal-body">
+			  <h5 class="text-center">Have I been here before?</h5>
+			  <p id="modal-mood" class="text-center">
+					<img class="btn btn-sm p-0 rounded-circle mx-auto position-relative visited-yes" width="50px" height="50px" src="./res/img/icons/success.svg">
+					<img class="btn btn-sm p-0 rounded-circle mx-auto position-relative visited-no" width="50px" height="50px" src="./res/img/icons/error.svg">
+				</p>
+			  <hr>
+			  <h5 class="text-center">Do I like this place?</h5>
+				<p class="text-center">
+					<img class="btn btn-sm p-0 rounded-circle mx-auto position-relative liked-yes" width="50px" height="50px" src="./res/img/icons/success.svg">
+					<img class="btn btn-sm p-0 rounded-circle mx-auto position-relative liked-no" width="50px" height="50px" src="./res/img/icons/error.svg">
+				</p>
+				<hr>
+				<h5 class="text-center">How does this place makes me feel?</h5>
+				<p class="text-center">
+					<div class="grid-container-modal">
+						<a id="adventurous" class="moodsConts adventMood modal-mood" >
+							<img class="compass" src="./res/img/icons/compass.png">
+							<img class="arrow" src="./res/img/icons/arrow.png">
+							<img class="traveler" src="./res/img/icons/traveler.png">
+						</a>
+						<a id="sad" class="moodsConts sadMood modal-mood" >
+							<img class="sadface" src="./res/img/icons/sadface.png">
+							<img class="tear" src="./res/img/icons/tear.png">
+						</a>
+						<a id="romantic" class="moodsConts romanticMood modal-mood" >
+							<img class="heart" src="./res/img/icons/heart.png">
+							<img class="couple" src="./res/img/icons/couple.png">
+						</a>
+						<a id="hungry" class="moodsConts hungryMood modal-mood" >
+							<img class="burger1" src="./res/img/icons/burger1.png">
+							<img class="burger2" src="./res/img/icons/burger2.png">
+							<img class="burger3" src="./res/img/icons/burger3.png">
+						</a>
+						<a id="tired" class="moodsConts tiredMood modal-mood" >
+							<div class="Zzz">
+								<span class="s2">z</span>
+								<span class="s3">z</span>
+								<span class="s4">z</span>
+								<span class="s5">z</span>
+								<span class="s6">z</span>
+							</div>
+							<img class="tiredMan" src="./res/img/icons/tiredMan.png">
+						</a>
+						<a id="creative" class="moodsConts creativeMood modal-mood" >
+							<img class="creativeFace" src="./res/img/icons/creativeFace.png">
+							<img class="gear1 spin" src="./res/img/icons/gear.png">
+							<img class="gear2 spin-back" src="./res/img/icons/gear.png">
+							<img class="gear3 spin" src="./res/img/icons/gear.png">
+						</a>
+					</div>
+				</p>
+			</div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+	<!-- MAP -->
 	<div id='map'></div>
+
+	<!-- Modal Script -->
+	<script type="text/javascript">
+	$('.visited-yes').on('click', e => {
+			data = JSON.parse(localStorage.getItem('data')) || json;
+			let zone = $(e.target).closest('.modal-content').find('#modal-place_name').attr('zone')
+			let place = $(e.target).closest('.modal-content').find('#modal-place_name').attr('place')
+			data[zone][0][place].isVisited = 'yes';
+			localStorage.setItem('data', JSON.stringify(data))
+
+			if(data[zone][0][place].isVisited === "yes") {//Checks from data if place has been visited and modifies the UI accordingly
+				$('.visited-yes').addClass('btn-success')
+				$('.visited-no').removeClass('btn-danger')
+			} else {
+				$('.visited-no').addClass('btn-danger')
+				$('.visited-yes').removeClass('btn-danger')
+			}
+		})
+
+		$('.visited-no').on('click', e => {
+				data = JSON.parse(localStorage.getItem('data')) || json;
+				let zone = $(e.target).closest('.modal-content').find('#modal-place_name').attr('zone')
+				let place = $(e.target).closest('.modal-content').find('#modal-place_name').attr('place')
+				data[zone][0][place].isVisited = 'no';
+				localStorage.setItem('data', JSON.stringify(data))
+
+				if(data[zone][0][place].isVisited === "yes") {//Checks from data if place has been visited and modifies the UI accordingly
+					$('.visited-yes').addClass('btn-success')
+					$('.visited-no').removeClass('btn-danger')
+				} else {
+					$('.visited-no').addClass('btn-danger')
+					$('.visited-yes').removeClass('btn-success')
+				}
+			})
+
+			$('.liked-yes').on('click', e => {
+					data = JSON.parse(localStorage.getItem('data')) || json;
+					let zone = $(e.target).closest('.modal-content').find('#modal-place_name').attr('zone')
+					let place = $(e.target).closest('.modal-content').find('#modal-place_name').attr('place')
+					data[zone][0][place].isVisited = 'yes';
+					localStorage.setItem('data', JSON.stringify(data))
+
+					if(data[zone][0][place].isVisited === "yes") {//Checks from data if place has been visited and modifies the UI accordingly
+						$('.liked-yes').addClass('btn-success')
+						$('.liked-no').removeClass('btn-danger')
+					} else {
+						$('.liked-no').addClass('btn-danger')
+						$('.liked-yes').removeClass('btn-danger')
+					}
+				})
+
+				$('.liked-no').on('click', e => {
+						data = JSON.parse(localStorage.getItem('data')) || json;
+						let zone = $(e.target).closest('.modal-content').find('#modal-place_name').attr('zone')
+						let place = $(e.target).closest('.modal-content').find('#modal-place_name').attr('place')
+						data[zone][0][place].isLiked = 'no';
+						localStorage.setItem('data', JSON.stringify(data))
+
+						if(data[zone][0][place].isLiked === "yes") {//Checks from data if place has been visited and modifies the UI accordingly
+							$('.liked-yes').addClass('btn-success')
+							$('.liked-no').removeClass('btn-danger')
+						} else {
+							$('.liked-no').addClass('btn-danger')
+							$('.liked-yes').removeClass('btn-success')
+						}
+					})
+	</script>
 </body>
 
 </html>

scripts/map.js
@@ -1,11 +1,12 @@
 let moodColour; //Obj containing mood:colour
 let data;       //Used to store a copy of json - so we modify the copy, not the original
-let adventurous, hungry, creative, tired, romantic, sad;
+let adventurous, hungry, creative, tired, romantic, sad; //Used to filter the markers
 
 function setup() {
-  data = json;
-  setIsLiked('central','hydepark', 'yes')
   noCanvas();
+  /* Data is the variable we will use to keep track of user preferences. E.g. if a user says he/she likes a place, it will be stored in 'data'. And this will be stored in localStorage*/
+  data = JSON.parse(localStorage.getItem('data')) || json; //Using localStorage 'data' if it exists, else use json (which is default data)
+  //localStorage.setItem('data', JSON.stringify(json)) //save 'data' to localStorage
   moodColour = {adventurous: "rgba(66, 173, 244, 0.65)", hungry: "rgba(118, 69, 209, 0.65)", creative: "rgba(229, 150, 22, 0.65)", tired: "rgba(56, 181, 97, 0.65)", romantic: "rgba(218, 118, 104, 0.65)", sad: "rgba(244, 205, 65, 0.65)"}
   let map = createMap(51.509865, -0.118092);//Creates a map centered in London center
   setupMoodFilter(map);
@@ -96,6 +97,38 @@ function setupMarkersByMood(map, mood) {//Creates and places markers based on mo
           let settings = document.createElement('img')
           settings.className = 'marker-icon'
           settings.src = '/res/img/icons/settings.svg'
+          $(settings).attr('data-toggle', 'modal');
+          $(settings).attr('data-target', '#changeSettings');
+          $(settings).on('click', e => {
+            $('#modal-place_name').html(placesObj[place].name) //Set modal name to place clicked
+            $('#modal-place_name').attr('zone', zone)
+            $('#modal-place_name').attr('place', place)
+
+
+            if(placesObj[place].isVisited === "yes") {//Checks from data if place has been visited and modifies the UI accordingly
+              $('.visited-yes').addClass('btn-success')
+              $('.visited-no').removeClass('btn-danger')
+            } else {
+              $('.visited-no').addClass('btn-danger')
+              $('.visited-yes').removeClass('btn-success')
+            }
+
+            if(placesObj[place].isLiked === "yes") {
+              $('.liked-yes').addClass('btn-success')
+              $('.liked-no').removeClass('btn-danger')
+            } else {
+              $('.liked-no').addClass('btn-danger')
+              $('.liked-yes').removeClass('btn-success')
+            }
+
+            Object.keys(moods).forEach(singleMood => {
+              $(`#${singleMood}`).addClass('hovered')
+              $('#modal-mood a').each((idx, el) => {
+                $(el).removeClass('hovered')
+              })
+            })
+
+          })
           icons.appendChild(settings);
 
           let showPlaceInfo = document.createElement('img')
@@ -162,7 +195,7 @@ function removeMarkers(markersArray) {//Removes all markers of a mood
 
 
 function setupMoodFilter(map) {
-  $('#adventMood').click(e => {
+  $('.adventMood').not('.modal-mood').click(e => {
     if (!$(e.target).closest('.moodsConts').hasClass('selected')) {
       adventurous = setupMarkersByMood(map, 'adventurous');
     }
@@ -177,7 +210,7 @@ function setupMoodFilter(map) {
     removeMarkers(tired);
   })
 
-  $('#sadMood').click(e => {
+  $('.sadMood').not('.modal-mood').click(e => {
     if (!$(e.target).closest('.moodsConts').hasClass('selected')) {
       sad = setupMarkersByMood(map, 'sad');
     }
@@ -192,7 +225,7 @@ function setupMoodFilter(map) {
     removeMarkers(tired);
   })
 
-  $('#romanticMood').click(e => {
+  $('.romanticMood').not('.modal-mood').click(e => {
     if (!$(e.target).closest('.moodsConts').hasClass('selected')) {
       romantic = setupMarkersByMood(map, 'romantic');
     }
@@ -207,7 +240,7 @@ function setupMoodFilter(map) {
     removeMarkers(tired);
   })
 
-  $('#tiredMood').click(e => {
+  $('.tiredMood').not('.modal-mood').click(e => {
     if (!$(e.target).closest('.moodsConts').hasClass('selected')) {
       tired = setupMarkersByMood(map, 'tired');
     }
@@ -222,7 +255,7 @@ function setupMoodFilter(map) {
     removeMarkers(adventurous);
   })
 
-  $('#hungryMood').click(e => {
+  $('.hungryMood').not('.modal-mood').click(e => {
     if (!$(e.target).closest('.moodsConts').hasClass('selected')) {
       hungry = setupMarkersByMood(map, 'hungry');
     }
@@ -237,7 +270,7 @@ function setupMoodFilter(map) {
     removeMarkers(tired);
   })
 
-  $('#creativeMood').click(e => {
+  $('.creativeMood').not('.modal-mood').click(e => {
     if (!$(e.target).closest('.moodsConts').hasClass('selected')) {
       creative = setupMarkersByMood(map, 'creative');
     }

style/app.css
@@ -16,51 +16,66 @@ h1{
   border-radius: 15px;
 }
 
-#adventMood {
+.home-btn {
+	position: absolute;
+	z-index: 5;
+	opacity: 0.7;
+}
+
+.home-btn > img {
+	width: 100px;
+	height: 100px;
+}
+
+.home-btn:hover {
+	opacity: .9;
+}
+
+.adventMood {
 	background-color: rgba(66, 173, 244, 0.65);
 }
 
-#adventMood:hover {
+.adventMood:hover, .adventMood.hovered  {
 	background-color: rgba(66, 173, 244, 0.85);
 }
 
-#sadMood {
+.sadMood {
 	background-color: rgba(244, 205, 65, 0.65);
 }
 
-#sadMood:hover {
+.sadMood:hover, .sadMood.hovered {
 	background-color: rgba(244, 205, 65, 0.85);
 }
 
-#romanticMood {
+.romanticMood {
 	background-color: rgba(218, 118, 104, 0.65);
 }
 
-#romanticMood:hover {
+.romanticMood:hover, .romanticMood.hovered {
 	background-color: rgba(218, 118, 104, 0.85);
 }
 
-#hungryMood {
+.hungryMood {
 	background-color: rgba(118, 69, 209, 0.65);
 }
 
-#hungryMood:hover {
+.hungryMood:hover, .hungryMood.hovered {
 	background-color: rgba(118, 69, 209, 0.85);
 }
 
-#tiredMood {
+.tiredMood {
 	background-color: rgba(56, 181, 97, 0.65);
 }
 
-#tiredMood:hover {
+.tiredMood:hover, .tiredMood.hovered {
 	background-color: rgba(56, 181, 97, 0.85);
 }
 
-#creativeMood {
+.creativeMood {
 	background-color: rgba(229, 150, 22, 0.65);
 }
 
-#creativeMood:hover {
+.creativeMood:hover, .creativeMood.hovered {
 	background-color: rgba(229, 150, 22, 0.85);
 }
 
@@ -72,13 +87,27 @@ h1{
 	grid-gap: 10px;
 	justify-content: center;
   z-index: 5;
-  transform: scale(0.3) translateY(-100%) translateX(-10%);
+  transform: scale(0.3) translateY(-100%) translateX(20vw);
   background: rgba(245, 245, 245, .9);
   padding: 3%;
   border-radius: 5%;
 }
 
-#sadface {
+.grid-container-modal {
+	position: absolute;
+	display: grid;
+	grid-template-columns: repeat(6, 220px);
+	grid-template-rows: 220px;
+	grid-gap: 10px;
+	justify-content: center;
+	z-index: 5;
+	transform: scale(0.3) translateY(-160%) translateX(-110%);
+	background: rgba(245, 245, 245, .9);
+	padding: 3%;
+	border-radius: 5%;
+}
+
+.sadface {
 	position: absolute;
 	width: 140px;
 	display: block;
@@ -86,7 +115,7 @@ h1{
   margin-top: 20px;
 }
 
-#tear {
+.tear {
 	position: absolute;
     width: 100px;
     height: 50px;
@@ -95,7 +124,7 @@ h1{
 	float: left;
 }
 
-#sadMood:hover > #tear { animation: crying 1.5s infinite; }
+.sadMood:hover > .tear, .sadMood.hovered > .tear { animation: crying 1.5s infinite; }
 
 @keyframes crying {
   from {
@@ -107,7 +136,7 @@ h1{
   }
 }
 
-#couple {
+.couple {
     width: 80px;
     height: 80px;
     position: relative;
@@ -118,7 +147,7 @@ h1{
     padding-right: 10px;
 }
 
-#heart {
+.heart {
     width: 205px;
     height: 205px;
     position: absolute;
@@ -126,7 +155,7 @@ h1{
 	margin-left: 5px;
 }
 
-#romanticMood:hover > #heart {
+.romanticMood:hover > .heart, .romanticMood.hovered > .heart {
 	animation: heartbeat 1s infinite;
 }
 
@@ -158,7 +187,7 @@ h1{
   }
 }
 
-#burger1, #burger2, #burger3 {
+.burger1, .burger2, .burger3 {
     width: 170px;
     height: 170px;
     position: absolute;
@@ -167,16 +196,16 @@ h1{
 
 }
 
-#hungryMood:hover > #burger1 {
+.hungryMood:hover > .burger1, .hungryMood.hovered > .burger1 {
 	animation: eating 4s infinite;
 }
 
-#hungryMood:hover > #burger2 {
+.hungryMood:hover > .burger2, .hungryMood.hovered > .burger2 {
 	animation: eating 4s infinite;
 	animation-delay: 1s;
 }
 
-#hungryMood:hover > #burger3 {
+.hungryMood:hover > .burger3, .hungryMood.hovered > .burger3 {
 	animation: eating 4s infinite;
 	animation-delay: 2s;
 }
@@ -196,7 +225,7 @@ h1{
   }
 }
 
-#tiredMan{
+.tiredMan{
 	width: 160px;
 	height: 160px;
 	display: block;
@@ -213,7 +242,7 @@ h1{
   position: absolute;
   opacity: 0;
   font-size: 35px;
-  color: #27590d;
+  color: .27590d;
   line-height: 15px;
 }
 
@@ -246,43 +275,43 @@ h1{
   100% {opacity:0;}
 }
 
-#creativeFace {
+.creativeFace {
 	width: 170px;
 	position: absolute;
 	margin-left: 30px;
     margin-top: 20px;
 }
 
-#tiredMood:hover > .Zzz span {
+.tiredMood:hover > .Zzz span, .tiredMood.hovered > .Zzz span {
 	animation: zzz 2s linear infinite;
 }
 
-#gear1, #gear2, #gear3 {
+.gear1, .gear2, .gear3 {
     width: 40px;
     height: 40px;
     position: absolute;
 }
 
-#gear1 {
+.gear1 {
     margin-left: 52px;
     margin-top: 73px;
 }
 
-#gear2 {
+.gear2 {
 	margin-left: 73px;
     margin-top: 40px;
 }
 
-#gear3 {
+.gear3 {
     margin-left: 112px;
     margin-top: 46px;
 }
 
-#creativeMood:hover > .spin {
+.creativeMood:hover > .spin, .creativeMood.hovered > .spin {
 	animation:spin 4s linear infinite;
 }
 
-#creativeMood:hover > .spin-back {
+.creativeMood:hover > .spin-back, .creativeMood.hovered > .spin-back {
 	animation:spin-back 4s linear infinite;
 }
 
@@ -294,7 +323,7 @@ h1{
 @keyframes spin-back { 100% { transform: rotate(-360deg); } }
 @keyframes spin-back { 100% { transform: rotate(-360deg); transform:rotate(-360deg); } }
 
-#compass {
+.compass {
     width: 140px;
     height: 155px;
     position: absolute;
@@ -302,7 +331,7 @@ h1{
     margin-top: 25px;
 }
 
-#traveler {
+.traveler {
     width: 100px;
     height: 120px;
     position: absolute;
@@ -310,7 +339,7 @@ h1{
     margin-left: 20px;
 }
 
-#arrow {
+.arrow {
     width: 40px;
     height: 90px;
     position: absolute;
@@ -318,7 +347,7 @@ h1{
     margin-top: 73px;
 }
 
-#adventMood:hover > #arrow {
+.adventMood:hover > .arrow, .adventMood.hovered > .arrow {
  	animation: rotateCompass 6s alternate infinite;
 }
 
@@ -354,7 +383,7 @@ h1{
 .slick-arrow {
   background-color: red;
 }
-.slick-arrow:hover {
+.slick-arrow:hover, .slick-arrow.hovered {
   background-color: blue;
 }
 
@@ -385,4 +414,8 @@ p {
   padding-left: 20px;
   padding-right: 20px;
   padding-bottom: 20px;
+}
+
+.modal-mood {
+	background: transparent;
 }

commit 8c04272654f07b6948ab8a70865ae8ac5efbcd8f

Improved mood filter  (#14)
* Added logic to forbid having places with no moods

* Added error message for the users

* Added zoom on hover to settings icon

* Added variable to reduce code redundancy

* Added evListener so custom style attr doesn't get stuck

* Fixed regression introduced with #12

index.html
@@ -146,6 +146,7 @@
 							<img class="gear3 spin" src="./res/img/icons/gear.png">
 						</a>
 					</div>
+					<span class="mood-error">Please select at least one mood per place.</span>
 				</p>
 			</div>
       <div class="modal-footer">
@@ -161,48 +162,64 @@
 	<!-- Modal Script -->
 	<script type="text/javascript">
 
-	function updateStat(stat, elem, value) {
-		$(elem).on('click', e => {
+	function updateStat(stat, query, elemY, elemN, value) {
+		$(query).on('click', e => {
 			let zone = $(e.target).closest('.modal-content').find('#modal-place_name').attr('zone')
 			let place = $(e.target).closest('.modal-content').find('#modal-place_name').attr('place')
 			data[zone][0][place][stat] = value;
 			localStorage.setItem('data', JSON.stringify(data))
 
 			if(data[zone][0][place][stat] === "yes") {//Checks from data if place has been visited and modifies the UI accordingly
-				$('.visited-yes').addClass('btn-success')
-				$('.visited-no').removeClass('btn-danger')
+				$(elemY).addClass('btn-success')
+				$(elemN).removeClass('btn-danger')
 			} else {
-				$('.visited-no').addClass('btn-danger')
-				$('.visited-yes').removeClass('btn-danger')
+				$(elemN).addClass('btn-danger')
+				$(elemY).removeClass('btn-success')
 			}
 		})
 	}
 
-	updateStat("isVisited", ".visited-yes", "yes");
-	updateStat("isVisited", ".visited-no", "no");
-	updateStat("isLiked", ".liked-yes", "yes");
-	updateStat("isLiked", ".liked-no", "no");
+	updateStat("isVisited",  ".visited-yes", ".visited-yes", ".visited-no", "yes");
+	updateStat("isVisited",  ".visited-no", ".visited-yes", ".visited-no", "no");
+	updateStat("isLiked",  ".liked-yes", ".liked-yes", ".liked-no", "yes");
+	updateStat("isLiked",  ".liked-no", ".liked-yes", ".liked-no", "no");
 
 
 	function updateMood(mood) {
 		$(`#${mood}`).on('click', e => {
 			let zone = $(e.target).closest('.modal-content').find('#modal-place_name').attr('zone')
 			let place = $(e.target).closest('.modal-content').find('#modal-place_name').attr('place')
+			let target = $(e.target).closest('.modal-mood');
 			if(data[zone][0][place]["mood"].hasOwnProperty(mood)) {
-				delete data[zone][0][place]["mood"][mood];
-				$(e.target).closest('.modal-mood').removeClass('hovered');
-				$(e.target).closest('.modal-mood').css('background', 'rgba(222, 20, 20, 0.5)')
-				$(e.target).closest('.modal-mood').mouseleave(e => {
-					$(e.target).removeAttr('style');
-				})
+				//If the place has MORE THAN 1 mood, then can delete. This is because if you delete all the moods from a place, it'll get lost in a limbo with no escape
+				if (Object.keys(data[zone][0][place]["mood"]).length > 1) {
+					delete data[zone][0][place]["mood"][mood];
+					$(target).removeClass('hovered');
+					$(target).css('background', 'rgba(222, 20, 20, 0.5)')
+					$(target).mouseleave(e => {
+						$(e.target).removeAttr('style');
+					})
+				} else {//If mood is 1 or less, do not delete and tell the user why he can't do that
+					$('.mood-error').css('display', 'initial')
+				}
 			} else {
 				data[zone][0][place]["mood"][mood] = 1;
-				$(e.target).closest('.modal-mood').addClass('hovered');
-				$(e.target).closest('.modal-mood').css('background', 'rgba(20, 222, 20, 0.5)')
-				$(e.target).closest('.modal-mood').mouseleave(e => {
+				$(target).addClass('hovered');
+				$(target).css('background', 'rgba(20, 222, 20, 0.5)')
+				$(target).mouseleave(e => {
 					$(e.target).removeAttr('style');
 				})
 			}
+
+			if (Object.keys(data[zone][0][place]["mood"]).length > 1) {
+				$('.mood-error').css('display', 'none') //Hide the error message
+			}
+
+			$('.modal-content').mousemove(() => {
+				$(target).removeAttr('style');
+			})
+
+
 			localStorage.setItem('data', JSON.stringify(data))
 		})
 	}

scripts/map.js
@@ -99,6 +99,11 @@ function setupMarkersByMood(map, mood) {//Creates and places markers based on mo
           settings.src = '/res/img/icons/settings.svg'
           $(settings).attr('data-toggle', 'modal');
           $(settings).attr('data-target', '#changeSettings');
+
+          $(settings).hover(e => { //Animate to signify click affordance
+            $(e.target).toggleClass('icon-hover')
+          })
+
           $(settings).on('click', e => {
             $('#modal-place_name').html(placesObj[place].name) //Set modal name to place clicked
             $('#modal-place_name').attr('zone', zone)

style/app.css
@@ -419,3 +419,15 @@ p {
 .modal-mood {
 	background: transparent;
 }
+
+.mood-error {
+	position: relative;
+	top: 50px;
+	left: 35px;
+	color: red;
+	display: none;
+}
+
+.icon-hover {
+	zoom: 1.1;
+}